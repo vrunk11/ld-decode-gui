356bfaa2310bf9d9b036cf0f5446e409161c6d76
vrunk11 <56720403+vrunk11@users.noreply.github.com>
2022-04-26 12:04:52 +0200

    Add functionality

--- ld_decode_utils.h
+++ ld_decode_utils.h
@@ -17,4 +17,44 @@ bool check_file_extention(String ext_list,String ext)
 	return false;
 }
 
+void generate_config()
+{
+	if(!FileExists("ld-decode-gui.cfg"))
+	{
+		String cfg;
+
+		cfg << "#the path of ld_analyse binary\n" << "path_ld_analyse=" << "\n\n";
+		cfg << "#launch ld_analyse at the end of the decode (default = 0)\n" << "launch_ld_analyse=" << "0" << "\n\n";
+		cfg << "#Default output folder in file selector\n" << "default_output_folder=" << "\n\n";
+		cfg << "#Default input folder in file selector\n" << "default_input_folder=" << "\n\n";
+		cfg << "#check the length option on startup\n" << "default_enable_length=" << "0" << "\n\n";
+		cfg << "#set the standard on startup\n" << "default_standard=" << "NTSC" << "\n\n";
+		cfg << "#check EFM option on startup\n" << "default_enable_efm=" << "1" << "\n\n";
+		cfg << "#check Analog audio option on startup\n" << "default_enable_analog_audio=" << "1" << "\n\n";
+		cfg << "#set the accepted input format\n" << "input_format=" << "s16,r16,u16,rf,r8,u8,lds,r30,raw.oga,ldf,wav,flac,vhs";
+
+		if(!SaveFile("ld-decode-gui.cfg", cfg))
+		{
+    		Exclamation("Error saving configuration!");
+		}
+	}
+}
+
+String read_file_content(String inputFile)
+{
+	FileStream cfg_file;
+        String file_content;
+        
+        cfg_file.Open(inputFile,BlockStream::READ);
+        
+        do 
+		{
+	        file_content += cfg_file.GetLine() + "\n";
+    	}while(cfg_file.IsEof() == false);
+       
+        cfg_file.Close();
+            	
+    	return file_content;
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -3,29 +3,96 @@
 #define LAYOUTFILE <ld_decode_gui/decode_gui.lay>
 #include <CtrlCore/lay.h>
 
+#define IMAGECLASS icon
+#define IMAGEFILE <ld_decode_gui/icon.iml>
+#include <Draw/iml.h>
+
 #include "ld_decode_utils.h"
 
 using namespace Upp;
 
 Thread thread1;
+VectorMap<String, String> cfg;
 
-VectorMap<String, String> cfg = LoadIniFile("ld-decode-gui.cfg");
+struct MyParametterWindow : public Withsetting<TopWindow> {
+
+    MyParametterWindow() {
+        CtrlLayout(*this, "LD DECODE SETTINGS");
+        
+    	//set the content of the file in the input text
+        text_config.SetData(read_file_content("ld-decode-gui.cfg"));
+        
+        btn_save  << [=] {
+	        if(!SaveFile("ld-decode-gui.cfg", text_config.GetData().ToString()))
+			{
+	    		Exclamation("Error saving configuration!");
+			}
+			else
+			{
+				PromptOK("Settings saved");
+			}
+        };
+        
+        btn_reset << [=] {
+            if(PromptOKCancel("You are about to reset settings ! Do you want to proced ?"))
+            {
+	            //rewrite config file
+	            generate_config();
+	            
+	            //set the content of the file in the input text
+	        	text_config.SetData(read_file_content("ld-decode-gui.cfg"));    
+            }
+       };
+    }
+};
 
 struct MyAppWindow : public Withdecode<TopWindow> {
+	MenuBar menu;
 	
     MyAppWindow() {
+        AddFrame(menu);
+
+        menu.Set([=](Bar& bar) {
+
+            bar.Sub("Menu", [=](Bar& bar) {
+
+                bar.Add("Settings",icon::settings, [=] {
+					MyParametterWindow().Run();
+                });
+
+            });
+
+        });
+         
         CtrlLayout(*this, "LD DECODE");
         
+        //set video standard
         standard_list.Add("NTSC");
         standard_list.Add("NTSCJ");
         standard_list.Add("PAL");
-        standard_list.SetIndex(0);
         
+        if(ToUpper(cfg.Get("default_standard")) == "NTSC"){
+        	standard_list.SetIndex(0);
+        }
+        else if(ToUpper(cfg.Get("default_standard")) == "NTSCJ"){
+        	standard_list.SetIndex(1);
+        }
+        else if(ToUpper(cfg.Get("default_standard")) == "NTSC-J"){
+            standard_list.SetIndex(1);
+        }
+        else if(ToUpper(cfg.Get("default_standard")) == "PAL"){
+            standard_list.SetIndex(2);
+        }
+        
+        cfg.Get("default_enable_efm") == "1" ? option_efm.Set(1) : option_efm.Set(0);
+        cfg.Get("default_enable_analog_audio") == "1" ? option_analog_audio.Set(1) : option_analog_audio.Set(0);
+        
+        // Start-seek option list
         option_start_list.Add("start");
         option_start_list.Add("seek");
         option_start_list.SetIndex(0);
         
-        cfg.Get("default_opt_length") == "1" ? option_length.Set(1) : option_length.Set(0) ;
+        cfg.Get("default_enable_length") == "1" ? option_length.Set(1) : option_length.Set(0) ;
         
         btn_input_file << [=] {
         	FileSel file;
@@ -38,9 +105,8 @@ struct MyAppWindow : public Withdecode<TopWindow> {
        		file.ExecuteOpen();
        		if(file.GetFile(0) != "")
        		{
-       			//PathFile = file.GetFile(0);
+       			label_file_name.SetText(GetFileName(file.GetFile(0)));
        			text_input_file.SetText(file.GetFile(0));
-       			//PromptOK("you have selected (" + PathFile + ")");
        		}
         };
         
@@ -72,6 +138,8 @@ struct MyAppWindow : public Withdecode<TopWindow> {
 			           String command;
 			           String opt_start;
 			           String opt_length;
+			           String opt_efm;
+			           String opt_analog_audio;
 			           
 			           String path_ld_analyse = cfg.Get("path_ld_analyse");
 			           String file_name = text_input_file.GetText().ToString();
@@ -79,14 +147,20 @@ struct MyAppWindow : public Withdecode<TopWindow> {
 			           
 			           //assign
 			           FileExists("ld-decode.py") ? (command = "ld-decode.py --") : (command = "ld-decode --");
-			           option_start_list.GetValue() == "start" && numeric_start.GetText().ToString() != ""  ? opt_start = " -s " + numeric_start.GetText().ToString() + " " : opt_start = " -S " + numeric_start.GetText().ToString() + " ";
+			           numeric_start.GetText().ToString() != "" ? (option_start_list.GetValue() == "start" ? opt_start = " -s " + numeric_start.GetText().ToString() + " " : opt_start = " -S " + numeric_start.GetText().ToString() + " ") : opt_start = "";
 			           option_length.Get() && numeric_length.GetText().ToString() != "" ? opt_length = " -l " + numeric_length.GetText().ToString() + " " : " ";
+			           !option_efm.Get() ? opt_efm = " --noEFM " : opt_efm = "";
+			           !option_analog_audio.Get() ? opt_analog_audio = " --disable_analog_audio " : opt_analog_audio = "";
 			           
 			           //composition command   
-			           command += standard_list.GetValue().ToString() + opt_start + opt_length + file_name + " " + text_output_folder.GetText().ToString();
+			           command += standard_list.GetValue().ToString() + opt_efm + opt_analog_audio + opt_start + opt_length + file_name + " " + text_output_folder.GetText().ToString();
 			           system("echo \"" + command + "\" & pause" );
 			           command = "echo \"" + command + "\" & " + command + " & pause" ;
-			           command += " && " + path_ld_analyse + " " + path_output_folder + "/" + file_name + ".tbc";
+			           
+			           if(cfg.Get("launch_ld_analyse") == "1")
+			           {
+			               command += " && " + path_ld_analyse + " " + path_output_folder + "/" + file_name + ".tbc";
+			           }
 			           //run
 			        	thread1.Start([=]{ system(command); });
 	               }
@@ -108,7 +182,7 @@ struct MyAppWindow : public Withdecode<TopWindow> {
     }
 };
 
-void read_config_test()
+/*void read_config_test()
 {
 	FileStream fConfig;
 	String cfg_test;
@@ -120,17 +194,20 @@ void read_config_test()
         PromptOK(cfg_test);
     }while(fConfig.IsEof() == false);
     fConfig.Close();
-}
+}*/
 
-void load_config()
+/*void load_config()
 {
 	VectorMap<String, String> cfg = LoadIniFile("ld-decode-gui.cfg");
-}
+}*/
 
 GUI_APP_MAIN
 {
+	generate_config();
+	//load config
+	cfg = LoadIniFile("ld-decode-gui.cfg");
 	//read_config_test();
-	if(!FileExists("ld-decode.py") && !FileExists("ld-decode")){PromptOK("ld-decode not found");}
+	if(!FileExists("ld-decode.py") && !FileExists("ld-decode")){Exclamation("ld-decode not found");}
     //run gui
     MyAppWindow().Run();
 }
